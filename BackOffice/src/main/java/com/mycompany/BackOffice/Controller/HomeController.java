dcd6cd78f94ab08a5fdefd7496fd78f04acbb79c
YENA <dpsk331@naver.com>
2021-11-22 17:49:47 +0900

    HYUN-12 <BackOffice 정적 페이지 구현>

---
+++ BackOffice/src/main/java/com/mycompany/BackOffice/Controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.mycompany.BackOffice.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j2;
+
+@Controller
+@Log4j2
+public class HomeController {
+	@RequestMapping("/")
+	public String home() {
+		log.info("Run home");
+		return "home";
+	}
+}
