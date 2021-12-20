c0114d246d62432883a398302de7c27314f9d680
Chenjae <injea95@naver.com>
2021-12-20 14:34:51 +0900

    HYUN-202 <주문리스트 조회 데이터베이스 수정 대응>

--- BackOffice/src/main/java/com/mycompany/BackOffice/dto/order/Payment.java
+++ BackOffice/src/main/java/com/mycompany/BackOffice/dto/order/Payment.java
@@ -5,10 +5,12 @@ import lombok.Data;
 @Data
 public class Payment{
 	String type;
+	String typeCode;
 	int price;
 	String accountNo;
 	String bank;
 	int installment;
 	String orderId;
 	String state;
+	String stateCode;
 }
